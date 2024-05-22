e7393e0d6631886aab84ac115332b5e0019fec50
Asit kumar <138616791+asitshakya789@users.noreply.github.com>
2024-05-22 22:15:44 +0530

    Add files via upload

---
+++ Learn the basic/if_else.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int age= 10;
+    if(age>=18){
+        cout<<"you are an adult"<<endl;
+    } else {
+        cout<<"You are not adult"<<endl;
+    }
+
+
+}
---
+++ Learn the basic/input_output1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    int y;
+    cin>> x>>y;
+    cout<<"Value of x:"<<x<< "and y"<<y;
+    return 0;
+    }
